8f42ae928c54f768c11117b1c9b00dff7184cb5e
world-11 <temavetrov1@mail.ru>
2024-11-09 20:12:53 +0300

    Initial commit

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+def welcome(request):
+    return render(request, 'func_template.html')
+
+class WELL(TemplateView):
+    template_name = 'class_template.html'
